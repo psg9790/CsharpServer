1f54f57b1993d2f2447e5b12be50ae3f80044355
psg9790 <psg9790@gmail.com>
2023-08-15 16:13:35 +0900

    sleep / yield

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -22,6 +22,11 @@ namespace ServerCore
                 int original = Interlocked.CompareExchange(ref _locked, desired, expected);
                 if (original == expected)
                     break;
+
+                // 쉬다 올게~
+                //Thread.Sleep(1);    // 무조건 휴식 => 무조건 1ms 정도 쉬고 싶어요
+                //Thread.Sleep(0);    // 조건부 양보 => 자신보다 우선순위가 낮은 애들한테는 양보 불가 => 우선순위가 나보다 같거나 높은 쓰레드가 없으면 다시 본인한테
+                Thread.Yield();     // 관대한 양보 => 관대하게 양보할 테니, 지금 실행 가능한 쓰레드가 있으면 실행하세요 => 실행 가능한 애가 없으면 남은 시간 소모
             }
         }
 
