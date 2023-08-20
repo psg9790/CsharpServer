3f1057d76c02a4ef83f3313f26981cef2ce70a70
psg9790 <psg9790@gmail.com>
2023-08-20 15:42:36 +0900

    session#3

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -16,16 +16,15 @@ namespace ServerCore
         {
             try
             {
-                Session session = new Session();
-                session.Start(clientSocket);    // Recv 자동화
+                Session session = new Session();    // 연결 시 세션 생성
+                session.Start(clientSocket);    // Recv 자동화, Send 대기
 
                 byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome To MMORPG Server!");
-                session.Send(sendBuff);
+                session.Send(sendBuff); // Send 요청
 
                 Thread.Sleep(1000);
                 session.Disconnect();
                 session.Disconnect();
-
             }
             catch (Exception e)
             {
@@ -41,7 +40,7 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            _listener.Init(endPoint, OnAcceptHandler);
+            _listener.Init(endPoint, OnAcceptHandler);  // recv를 받으면 자동으로 OnAcceptHandler
 
             Console.WriteLine("Listening...");
             while (true)
--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -13,37 +13,99 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;
 
+        object _lock = new object();
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs _recvArgs = new SocketAsyncEventArgs();
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+
+
         public void Start(Socket socket)
         {
             _socket = socket;
 
-            SocketAsyncEventArgs recvArgs = new SocketAsyncEventArgs();
-            recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
-            recvArgs.SetBuffer(new byte[1024], 0, 1024);
+            _recvArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            _recvArgs.SetBuffer(new byte[1024], 0, 1024);
 
-            RegisterRecv(recvArgs);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
+            RegisterRecv();
         }
 
         public void Send(byte[] sendBuff)
         {
-            _socket.Send(sendBuff);
+            lock (_lock)
+            {
+                _sendQueue.Enqueue(sendBuff);   // send 할 내용을 큐에 넣는다
+                if (_pendingList.Count == 0)  // 작업 중이지 않으면
+                {
+                    RegisterSend(); // 전송 등록한다
+                }
+            }
         }
+
         public void Disconnect()
         {
             if (Interlocked.Exchange(ref _disconnected, 1) == 1)    // 멀티쓰레드 방지
                 return;
             _socket.Shutdown(SocketShutdown.Both);
             _socket.Close();
-
         }
 
         #region 네트워크통신
-        void RegisterRecv(SocketAsyncEventArgs args)
+
+        void RegisterSend()
         {
-            bool pending = _socket.ReceiveAsync(args);
+            while (_sendQueue.Count > 0)
+            {
+                byte[] buff = _sendQueue.Dequeue();
+                _pendingList.Add(new ArraySegment<byte>(buff, 0, buff.Length));
+            }
+            _sendArgs.BufferList = _pendingList;
+
+            bool pending = _socket.SendAsync(_sendArgs);    // 비동기 send를 시작한다
+            if (pending == false)   // 즉시 완료할 수 있다면
+            {
+                OnSendCompleted(null, _sendArgs);   // send 완료 작업을 바로 수행한다.
+            }
+
+        }
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();
+
+                        Console.WriteLine($"Transffered bytes: {_sendArgs.BytesTransferred}");
+
+                        if (_sendQueue.Count > 0)   // 추가된 작업이 더 있으면
+                        {
+                            RegisterSend(); // 자동으로 추가 전송
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
+        void RegisterRecv()
+        {
+            bool pending = _socket.ReceiveAsync(_recvArgs);
             if (pending == false)
             {
-                OnRecvCompleted(null, args);
+                OnRecvCompleted(null, _recvArgs);
             }
         }
 
@@ -56,7 +118,7 @@ namespace ServerCore
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client] {recvData}");
 
-                    RegisterRecv(args);
+                    RegisterRecv();
                 }
                 catch (Exception e)
                 {
@@ -65,7 +127,7 @@ namespace ServerCore
             }
             else
             {
-                // TODO disconnect
+                Disconnect();
             }
         }
         #endregion
