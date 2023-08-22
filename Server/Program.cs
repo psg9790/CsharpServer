edb3c25a7fb353a64cfd402cff5a8bb7191f8741
psg9790 <psg9790@gmail.com>
2023-08-22 14:51:54 +0900

    packetSession

--- Server/Program.cs
+++ Server/Program.cs
@@ -8,44 +8,46 @@ using ServerCore;
 
 namespace Server
 {
-    class Knight
+    class Packet
     {
-        public int hp;
-        public int attack;
+        public ushort size;
+        public ushort packetId;
     }
-    class GameSession : Session
+
+    class GameSession : PacketSession
     {
         public override void OnConnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnConnected: {endPoint}");
             //byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome To MMORPG Server!");
-            Knight knight = new Knight(){ hp = 100, attack = 10 };
-
+            //Packet packet = new Packet() { size = 100, packetId = 10 };
+/*
             ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-            byte[] buffer = BitConverter.GetBytes(knight.hp);
-            byte[] buffer2 = BitConverter.GetBytes(knight.attack);
+            byte[] buffer = BitConverter.GetBytes(packet.size);
+            byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
             Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
             Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
             ArraySegment<byte> sendBuff = SendBufferHelper.Close(buffer.Length + buffer2.Length);
-
-            Send(sendBuff); // Send 요청
-            Thread.Sleep(1000);
+*/
+            //Send(sendBuff); // Send 요청
+            Thread.Sleep(5000);
             Disconnect();
         }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
+            ushort id = BitConverter.ToUInt16(buffer.Array, buffer.Offset + 2);
+            Console.WriteLine($"recv packet id: {id}, size: {size}");
+
+        }
+
 
         public override void OnDisconnected(EndPoint endPoint)
         {
             Console.WriteLine($"OnDisconnected: {endPoint}");
 
         }
-
-        public override int OnRecv(ArraySegment<byte> buffer)
-        {
-            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
-            Console.WriteLine($"[From Client] {recvData}");
-            return buffer.Count;
-        }
-
         public override void OnSend(int numOfBytes)
         {
             Console.WriteLine($"Transffered bytes: {numOfBytes}");
